40b5bcd6c9034c5392402ff651ef1084725b0c8f
Ikhwan <ikhwan220397@gmail.com>
2022-10-31 13:26:21 +0700

    [add] TweenAnimationBuilder

--- README.md
+++ README.md
@@ -9,21 +9,21 @@ Explore All Widgets and Layout Behavior
 - Column, Row, Stack, Wrap
 - Expanded, Expanded with flex
 - Inherited Widget, Inherited Model
-- Animation (AnimatedAlign, AnimatedContainer, AnimatedDefaultTextStyle, AnimatedOpacity, AnimatedPadding, AnimatedPhysicalModel, AnimatedPositioned, AnimatedPositionedDirectional, AnimatedSize)
+- Animation (AnimatedAlign, AnimatedContainer, AnimatedDefaultTextStyle, AnimatedOpacity, AnimatedPadding, AnimatedPhysicalModel, AnimatedPositioned, AnimatedPositionedDirectional, AnimatedSize, TweenAnimationBuilder)
 - Auto Complete
 - Button
 - ClipRect
 - FAB & BottomNav
 - Fade In Image
-- Font: [flutter.dev](https://flutter.dev/docs/cookbook/design/fonts)
+- [Font](https://flutter.dev/docs/cookbook/design/fonts)
 - Form (TextFormField, DropdownButton, DropdownButtonFormField, Switch, CheckBox)
 - Hero
 - NavigationRail
-- ListView.Builder with Click: [api.flutter.dev](https://api.flutter.dev/flutter/widgets/ListView/ListView.builder.html)
-- ListView.Builder with Custom Click: [api.flutter.dev](https://api.flutter.dev/flutter/widgets/ListView/ListView.builder.html)
+- [ListView.Builder with Click](https://api.flutter.dev/flutter/widgets/ListView/ListView.builder.html)
+- [ListView.Builder with Custom Click](https://api.flutter.dev/flutter/widgets/ListView/ListView.builder.html)
 - Loading
 - Opacity
-- PageView: [api.flutter.dev](https://api.flutter.dev/flutter/widgets/PageView-class.html)
+- [PageView](https://api.flutter.dev/flutter/widgets/PageView-class.html)
 - RepaintBoundary
 - Safe Area
 - Sliver App Bar, Sliver List 
@@ -39,7 +39,7 @@ Explore All Widgets and Layout Behavior
 
 <h3>pub.dev</h3>
 
-- flutter_rating_bar: [link](https://pub.dev/packages/flutter_rating_bar)
+- [flutter_rating_bar](https://pub.dev/packages/flutter_rating_bar)
 
 <h2>🔭 On Progress / todo list </h2>
 
--- lib/widget/animation/animationActivity.dart
+++ lib/widget/animation/animationActivity.dart
@@ -36,6 +36,14 @@ class _AnimationActivityState extends State<AnimationActivity> {
               ),
             ),
             ..._getAnimatedFooWidgets(context),
+            MySubTitle(
+              "TweenAnimationBuilder widgets",
+              margin: EdgeInsets.only(
+                top: 4,
+                bottom: 8,
+              ),
+            ),
+            MyTweenAnimationBuilder()
           ],
         ),
       ),
@@ -612,6 +620,125 @@ class _MyAnimatedSize extends State<MyAnimatedSize> {
                 child: FlutterLogo(size: _size),
               ),
             ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+///
+class MyTweenAnimationBuilder extends StatefulWidget {
+  @override
+  State<MyTweenAnimationBuilder> createState() => _MyTweenAnimationBuilder();
+}
+
+class _MyTweenAnimationBuilder extends State<MyTweenAnimationBuilder> {
+  double targetValue = 24.0;
+  Color _color = Colors.blue;
+  double _currentSliderValue = 0;
+  Color _colorSlider = Colors.red;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        MySubTitle(
+          "TweenAnimationBuilder",
+          color: Colors.lightBlueAccent,
+        ),
+        Container(
+          margin: contentMargin,
+          // height: 170.0,
+          child: Column(
+            children: [
+              TweenAnimationBuilder<double>(
+                tween: Tween<double>(begin: 0, end: targetValue),
+                duration: const Duration(seconds: 1),
+                builder: (BuildContext context, double size, Widget? child) {
+                  return IconButton(
+                    iconSize: size,
+                    color: Colors.blue,
+                    icon: child!,
+                    onPressed: () {
+                      setState(() {
+                        targetValue = targetValue == 24.0 ? 48.0 : 24.0;
+                      });
+                    },
+                  );
+                },
+                child: const Icon(Icons.aspect_ratio),
+              ),
+
+              /// Tween<T extends Object?>
+              /// ReverseTween<T extends Object?>
+              /// ColorTween
+              /// SizeTween
+              /// RectTween
+              /// IntTween
+              /// StepTween
+              /// ConstantTween
+              /// CurveTween
+              TweenAnimationBuilder<Color?>(
+                tween: ColorTween(begin: Colors.red, end: _color),
+                duration: const Duration(seconds: 1),
+                builder: (BuildContext context, Color? color, Widget? child) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: color,
+                    ),
+                    child: child,
+                    onPressed: () {
+                      setState(() {
+                        if (_color == Colors.green) {
+                          _color = Colors.blue;
+                        } else {
+                          _color = Colors.green;
+                        }
+                      });
+                    },
+                  );
+                },
+                child: const Text("Change Color"),
+              ),
+              TweenAnimationBuilder<Color?>(
+                tween: ColorTween(begin: Colors.red, end: _colorSlider),
+                duration: const Duration(seconds: 1),
+                builder: (BuildContext context, Color? color, Widget? child) {
+                  return SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      trackShape: RectangularSliderTrackShape(),
+                      trackHeight: 8.0,
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 12.0),
+                    ),
+                    child: Slider(
+                      value: _currentSliderValue,
+                      max: 100,
+                      activeColor: color,
+                      thumbColor: color,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _currentSliderValue = value;
+                            if (value >= 0 && value < 25) {
+                              _colorSlider = Colors.red;
+                            } else if (value >= 25 && value < 50) {
+                              _colorSlider = Colors.orange;
+                            } else if (value >= 50 && value < 75) {
+                              _colorSlider = Colors.yellow;
+                            } else if (value >= 75 && value <= 100) {
+                              _colorSlider = Colors.green;
+                            }
+                          },
+                        );
+                      },
+                    ),
+                  );
+                },
+              )
+            ],
           ),
         ),
       ],
