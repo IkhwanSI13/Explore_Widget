7668bf24d86c9b6744f0e01164e4a672b749fbb7
Ikhwan <ikhwan220397@gmail.com>
2022-12-13 13:32:20 +0700

    explore rive

--- lib/pubDev/rive/riveActivity.dart
+++ lib/pubDev/rive/riveActivity.dart
@@ -25,17 +25,20 @@ class _RiveActivityState extends State<RiveActivity> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             ///
-            // possible source:
-            // asset, file, and network,
-            // MyTitle("Simple load"),
-            // SizedBox(
-            //   height: 120,
-            //   child: RiveAnimation.asset(
-            //     'assets/rive/thanks.riv',
-            //     fit: BoxFit.cover,
-            //   ),
-            // ),
-            MyTitle("With Controller"),
+            MyTitle("RiveAnimation.asset | animations"),
+            SizedBox(
+              child: WolvieRive(),
+            ),
+            MyTitle(
+                "RiveAnimation.asset | RiveAnimationController | SimpleAnimation"),
+            SizedBox(
+              child: WolvieRive2(),
+            ),
+            MyTitle("RiveAnimation.asset | SimpleAnimation"),
+            SizedBox(
+              child: StickManRiveOneShoot(),
+            ),
+            MyTitle("StateMachineController"),
             SizedBox(
               height: 281,
               child: StateMachineSkills(),
@@ -44,17 +47,193 @@ class _RiveActivityState extends State<RiveActivity> {
               height: 281,
               child: CarRive(),
             ),
-            SizedBox(
-              height: 281,
-              child: StickmanRive(),
-            ),
-            SizedBox(
-              height: 359,
-              child: WolvieRive(),
-            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+///
+class WolvieRive extends StatefulWidget {
+  const WolvieRive({Key? key}) : super(key: key);
+
+  @override
+  _WolvieRiveState createState() => _WolvieRiveState();
+}
+
+class _WolvieRiveState extends State<WolvieRive> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(
+          width: 120,
+          height: 120,
+          child: const RiveAnimation.asset(
+            "assets/rive/wolvie.riv",
+            animations: ["walk", "berserkerRage"],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+///
+class WolvieRive2 extends StatefulWidget {
+  const WolvieRive2({Key? key}) : super(key: key);
+
+  @override
+  _WolvieRiveState2 createState() => _WolvieRiveState2();
+}
+
+class _WolvieRiveState2 extends State<WolvieRive2> {
+  late RiveAnimationController _controller;
+  late RiveAnimationController _controllerWalk;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = SimpleAnimation('berserkerRage');
+    _controllerWalk = SimpleAnimation('walk');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(
+          width: 120,
+          height: 120,
+          child: RiveAnimation.asset(
+            "assets/rive/wolvie.riv",
+            controllers: [
+              _controllerWalk,
+              _controller,
+            ],
+          ),
+        ),
+        Wrap(
+          runSpacing: 10.0,
+          spacing: 10.0,
+          children: [
+            ElevatedButton(
+                child: const Text('Berserker Rage'),
+                onPressed: () {
+                  setState(() {
+                    print(
+                        "Ikhwan controller Berserker before: ${_controllerWalk.isActive}");
+                    _controller.isActive = !_controller.isActive;
+                    print(
+                        "Ikhwan controller Berserker after: ${_controllerWalk.isActive}");
+                  });
+                }),
+            ElevatedButton(
+              child: const Text('Walk'),
+              onPressed: () {
+                setState(() {
+                  print(
+                      "Ikhwan controller walk before: ${_controllerWalk.isActive}");
+                  _controllerWalk.isActive = !_controllerWalk.isActive;
+                  print(
+                      "Ikhwan controller walk after: ${_controllerWalk.isActive}");
+                });
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+///
+class StickManRiveOneShoot extends StatefulWidget {
+  const StickManRiveOneShoot({Key? key}) : super(key: key);
+
+  @override
+  _StickManRiveOneShootState createState() => _StickManRiveOneShootState();
+}
+
+///
+class _StickManRiveOneShootState extends State<StickManRiveOneShoot> {
+  /// Controller for playback
+  late RiveAnimationController _controller;
+  late RiveAnimationController _controller2;
+
+  /// Is the animation currently playing?
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = SimpleAnimation('Run');
+    _controller2 = SimpleAnimation('Run_into_punch');
+    // _controller = SimpleAnimation('isRunning');
+    // _controller = OneShotAnimation(
+    //   'crossPunch',
+    //   autoplay: false,
+    //   onStop: () => setState(() => _isPlaying = false),
+    //   onStart: () => setState(() => _isPlaying = true),
+    // );
+
+    //          _crossPunch = controller.findInput('crossPunch');
+    //           _jabPunch = controller.findInput('jabPunch');
+    //           _isRunning = controller.findInput('isRunning');
+    //           _sideKick = controller.findInput('sideKick');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(
+          width: 120,
+          height: 120,
+          child: RiveAnimation.asset(
+            "assets/rive/character.riv",
+            controllers: [_controller, _controller2],
+            animations: ["Idle"],
+          ),
+        ),
+        Wrap(
+          runSpacing: 10.0,
+          spacing: 10.0,
+          children: [
+            ElevatedButton(
+              child: const Text('crossPunch'),
+              onPressed: () {
+                // setState(() {
+                //   _isPlaying ? null : _controller.isActive = true;
+                // });
+                setState(() {
+                  print(
+                      "Ikhwan controller cross punch before: ${_controller.isActive}");
+                  _controller.isActive = !_controller.isActive;
+                  print(
+                      "Ikhwan controller cross punch after: ${_controller.isActive}");
+                });
+              },
+            ),
+            ElevatedButton(
+              child: const Text('run punch'),
+              onPressed: () {
+                setState(() {
+                  print(
+                      "Ikhwan controller _controller2.isActive before: ${_controller2.isActive}");
+                  _controller2.isActive = !_controller2.isActive;
+                  print(
+                      "Ikhwan controller _controller2.isActive after: ${_controller2.isActive}");
+                });
+              },
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
@@ -199,162 +378,6 @@ class _CarRiveState extends State<CarRive> {
                   ElevatedButton(
                     child: const Text('Speed Up'),
                     onPressed: () => _isPressed!.value = !_isPressed!.value,
-                  ),
-                ],
-              ),
-            ],
-          );
-  }
-}
-
-class StickmanRive extends StatefulWidget {
-  const StickmanRive({Key? key}) : super(key: key);
-
-  @override
-  _StickmanRiveState createState() => _StickmanRiveState();
-}
-
-class _StickmanRiveState extends State<StickmanRive> {
-  Artboard? _riveArtboard;
-  SMIInput<bool>? _crossPunch;
-  SMIInput<bool>? _jabPunch;
-  SMIInput<bool>? _isRunning;
-  SMIInput<bool>? _sideKick;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Load the animation file from the bundle, note that you could also
-    // download this. The RiveFile just expects a list of bytes.
-    rootBundle.load('assets/rive/character.riv').then(
-      (data) async {
-        // Load the RiveFile from the binary data.
-        final file = RiveFile.import(data);
-
-        // The artboard is the root of the animation and gets drawn in the
-        // Rive widget.
-        final artboard = file.mainArtboard;
-        var controller =
-            StateMachineController.fromArtboard(artboard, 'New Artboard');
-        if (controller != null) {
-          artboard.addController(controller);
-          _crossPunch = controller.findInput('crossPunch');
-          _jabPunch = controller.findInput('jabPunch');
-          _isRunning = controller.findInput('isRunning');
-          _sideKick = controller.findInput('sideKick');
-        }
-        setState(() => _riveArtboard = artboard);
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _riveArtboard == null
-        ? const SizedBox()
-        : Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Rive(
-                artboard: _riveArtboard!,
-                fit: BoxFit.cover,
-                useArtboardSize: true,
-              ),
-              Wrap(
-                spacing: 10.0,
-                children: [
-                  ElevatedButton(
-                    child: const Text('Cross Punch'),
-                    onPressed: () => _crossPunch!.value = !_crossPunch!.value,
-                  ),
-                  ElevatedButton(
-                    child: const Text('Jab Punch'),
-                    onPressed: () => _jabPunch!.value = !_jabPunch!.value,
-                  ),
-                  ElevatedButton(
-                    child: const Text('Run'),
-                    onPressed: () => _isRunning!.value = !_isRunning!.value,
-                  ),
-                  ElevatedButton(
-                    child: const Text('Side Kick'),
-                    onPressed: () => _sideKick!.value = !_sideKick!.value,
-                  ),
-                ],
-              ),
-            ],
-          );
-  }
-}
-
-class WolvieRive extends StatefulWidget {
-  const WolvieRive({Key? key}) : super(key: key);
-
-  @override
-  _WolvieRiveState createState() => _WolvieRiveState();
-}
-
-class _WolvieRiveState extends State<WolvieRive> {
-  Artboard? _riveArtboard;
-  SMIInput<bool>? _berserkerRage;
-  SMIInput<bool>? _direction;
-  SMIInput<bool>? _walk;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Load the animation file from the bundle, note that you could also
-    // download this. The RiveFile just expects a list of bytes.
-    rootBundle.load('assets/rive/wolvie.riv').then(
-      (data) async {
-        // Load the RiveFile from the binary data.
-        final file = RiveFile.import(data);
-
-        // The artboard is the root of the animation and gets drawn in the
-        // Rive widget.
-        final artboard = file.mainArtboard;
-        var controller =
-            StateMachineController.fromArtboard(artboard, 'Animate');
-        if (controller != null) {
-          artboard.addController(controller);
-          _berserkerRage = controller.findInput('berserkerRage');
-          _direction = controller.findInput('direction(defaultRight)');
-          _walk = controller.findInput('walk');
-        }
-        setState(() => _riveArtboard = artboard);
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _riveArtboard == null
-        ? const SizedBox()
-        : Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Rive(
-                artboard: _riveArtboard!,
-                fit: BoxFit.cover,
-                useArtboardSize: true,
-              ),
-              Wrap(
-                runSpacing: 10.0,
-                spacing: 10.0,
-                children: [
-                  ElevatedButton(
-                    child: const Text('Berserker Rage'),
-                    onPressed: () =>
-                        _berserkerRage!.value = !_berserkerRage!.value,
-                  ),
-                  ElevatedButton(
-                    child: const Text('Direction'),
-                    onPressed: () => _direction!.value = !_direction!.value,
-                  ),
-                  ElevatedButton(
-                    child: const Text('Walk'),
-                    onPressed: () => _walk!.value = !_walk!.value,
                   ),
                 ],
               ),
